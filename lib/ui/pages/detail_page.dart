d63727e36d0d0e14d43947af5a6011b09a71d885
LuckyMaulana24 <luckymaulana35@gmail.com>
2022-11-12 21:43:49 +0700

    menambahkan statelessWidget di detailpage

--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -1,2 +1,11 @@
 import 'package:flutter/material.dart';
 
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
