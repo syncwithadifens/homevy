a8c5e278ac2d2b4a8ed02281a5317d3522e0bb37
LuckyMaulana24 <luckymaulana35@gmail.com>
2022-11-12 21:51:56 +0700

    appbar

--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -6,8 +6,9 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: const Text("Detail Page"),
+      ),
     );
   }
 }
-
