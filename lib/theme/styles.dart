20c90246f39b099b258f2f74602e30a87ff265cd
Afiv Dicky Efendy <dickyareda@gmail.com>
2022-11-13 13:16:35 +0700

    menambahkan style theme

--- lib/theme/styles.dart
+++ lib/theme/styles.dart
@@ -3,6 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 
 Color primaryColor = const Color(0xffFCA311);
 Color secondaryColor = const Color(0xff14213D);
+Color whiteColor = Colors.white;
+Color blackColor = Colors.black;
+Color greyColor = const Color(0xffF1F0F5);
 
 TextStyle titleStyle =
     GoogleFonts.concertOne(fontSize: 20, color: primaryColor);
