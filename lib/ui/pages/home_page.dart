167a960e96562397b677f03172c788aeff2ad48e
AngelicaPngt <nglcpanget@gmail.com>
2022-11-13 19:25:34 +0700

    coba

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text('coba'),
+      body: Text('Home'),
     );
   }
 }
