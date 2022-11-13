cf59bcdcbdd994f7cf88bf794e3fc2958a5c351b
Afiv Dicky Efendy <dickyareda@gmail.com>
2022-11-13 11:00:04 +0700

    menyelesaikan halaman splash screen

---
+++ lib/theme/styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = const Color(0xffFCA311);
+Color secondaryColor = const Color(0xff14213D);
+
+TextStyle titleStyle =
+    GoogleFonts.concertOne(fontSize: 20, color: primaryColor);
+TextStyle subtitleStyle = GoogleFonts.nunito(fontSize: 16);
+TextStyle taglineStyle =
+    GoogleFonts.pacifico(fontSize: 18, color: secondaryColor);
--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:homevy/theme/styles.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'HOMEVY',
+              style: titleStyle.copyWith(fontSize: 48),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Text(
+                'your perfect room',
+                style: taglineStyle,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
