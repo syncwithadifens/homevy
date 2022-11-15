1439fc1187335264643381b4a0ef2c89f0c005d2
LuckyMaulana24 <luckymaulana35@gmail.com>
2022-11-15 17:32:53 +0700

    menambahkan bottom navigation bar di hompage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:homevy/ui/pages/home_page.dart';
 import 'package:homevy/ui/pages/splash_page.dart';
 
 void main() async {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      home: HomePage(),
     );
   }
 }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:homevy/theme/styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Home'),
+    return Scaffold(
+      backgroundColor: bgColor,
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: blackColor),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: blackColor,
+              ),
+              label: 'Wishlist',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.card_travel,
+                color: blackColor,
+              ),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.people,
+                color: blackColor,
+              ),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+      body: const Text('Home'),
     );
   }
 }
