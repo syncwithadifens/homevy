9e40089c788eede2490ede8651b0e91627269fca
Lucky Maulana <54613207+LuckyMaulana24@users.noreply.github.com>
2022-11-15 17:49:26 +0700

    Update styles.dart

--- lib/theme/styles.dart
+++ lib/theme/styles.dart
@@ -6,13 +6,9 @@ Color secondaryColor = const Color(0xff14213D);
 Color whiteColor = Colors.white;
 Color blackColor = Colors.black;
 Color lightGreyColor = const Color(0xffF1F0F5);
-<<<<<<< HEAD
 Color greyColor = const Color(0xffA2A6AE);
 Color bgColor = const Color(0xffF3F3F3);
-=======
-Color greyColor = Colors.grey;
-Color backgroundColor = const Color(0xffF3F3F3);
->>>>>>> 03d5848d742096104feaa32d0ec575eaa1937887
+
 
 TextStyle titleStyle = GoogleFonts.concertOne(
   fontSize: 20,
@@ -21,7 +17,6 @@ TextStyle titleStyle = GoogleFonts.concertOne(
 TextStyle subtitleStyle = GoogleFonts.nunito(fontSize: 16);
 TextStyle taglineStyle =
     GoogleFonts.pacifico(fontSize: 18, color: secondaryColor);
-<<<<<<< HEAD
 
 TextStyle blackTextStyle = GoogleFonts.nunito(
   color: blackColor,
@@ -41,5 +36,3 @@ FontWeight regular = FontWeight.w400;
 FontWeight medium = FontWeight.w500;
 FontWeight semobold = FontWeight.w600;
 FontWeight bold = FontWeight.w700;
-=======
->>>>>>> 03d5848d742096104feaa32d0ec575eaa1937887
