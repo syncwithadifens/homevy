f47ef2d99aac3606b8ca009291f07fe9fa1c0b4f
LuckyMaulana24 <luckymaulana35@gmail.com>
2022-11-15 16:57:04 +0700

    menambahkan color dan style baru

--- lib/theme/styles.dart
+++ lib/theme/styles.dart
@@ -6,13 +6,32 @@ Color secondaryColor = const Color(0xff14213D);
 Color whiteColor = Colors.white;
 Color blackColor = Colors.black;
 Color lightGreyColor = const Color(0xffF1F0F5);
-Color greyColor = Colors.grey;
-Color backgroundColor = Color(0xffF3F3F3);
+Color greyColor = const Color(0xffA2A6AE);
+Color bgColor = const Color(0xffF3F3F3);
 
-TextStyle titleStyle =
-    GoogleFonts.concertOne(fontSize: 20, color: primaryColor);
+TextStyle titleStyle = GoogleFonts.concertOne(
+  fontSize: 20,
+  color: primaryColor,
+);
 TextStyle subtitleStyle = GoogleFonts.nunito(fontSize: 16);
 TextStyle taglineStyle =
     GoogleFonts.pacifico(fontSize: 18, color: secondaryColor);
 
+TextStyle blackTextStyle = GoogleFonts.nunito(
+  color: blackColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.nunito(
+  color: whiteColor,
+);
+TextStyle yellowTextStyle = GoogleFonts.nunito(
+  color: primaryColor,
+);
+TextStyle greyTextStyle = GoogleFonts.nunito(
+  color: greyColor,
+);
 
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semobold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
