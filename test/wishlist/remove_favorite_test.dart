7a0e2f99f6c39a979547c24ff87c86a85007ac47
Afiv Dicky Efendy <dickyareda@gmail.com>
2022-12-02 11:43:44 +0700

    update

--- test/wishlist/remove_favorite_test.dart
+++ test/wishlist/remove_favorite_test.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:homevy/controllers/wishlist_controller.dart';
 
 void main() {
-  Map favoriteItem = {1: 20, 2: 30, 3: 15, 4: 8};
+  WishlistController wishlistController = WishlistController();
   test(
     'remove product from wishlist',
     () {
-      favoriteItem.remove(2);
-      expect(favoriteItem.length, equals(3));
+      wishlistController.favoriteItem.addAll({22: 60, 23: 61, 24: 62});
+      wishlistController.favoriteItem.remove(22);
+      expect(wishlistController.favoriteItem.length, equals(2));
     },
   );
 }
